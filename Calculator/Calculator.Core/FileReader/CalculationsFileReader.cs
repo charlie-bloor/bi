2922aaea549a21f234b24e9d6e85be8ad0b17c9e
Charlie Bloor <charlie@nit5.com>
2021-06-16 21:55:53 +0100

    Commit CalculationsFileReader as WIP

--- Calculator/Calculator.Core/FileReader/CalculationsFileReader.cs
+++ Calculator/Calculator.Core/FileReader/CalculationsFileReader.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
-using Calculator.Core.Converters;
 
 namespace Calculator.Core.FileReader
 {
@@ -18,34 +16,85 @@ namespace Calculator.Core.FileReader
         {
             _operationFactory = operationFactory;
         }
-        
+
         // TODO: make cancellable
         public async IAsyncEnumerable<Operation> GetOrderedOperations(FileInfo fileInfo)
         {
             // Handle file problems
             // yield return convert GetLastLine
             // for each remaining line, yield return
-            
+
+            // Return the last instruction first
+            var lastLineOfFile = GetLastLineOfFile(fileInfo);
+            var operation = _operationFactory.Create(lastLineOfFile);
+
+            if (operation.OperationType != OperationType.Apply)
+            {
+                // TODO: throw
+            }
+
+            yield return operation;
+
             string nextLine;
-            var lineCounter = 1;  // TODO: use
+            var lineCounter = 1; // TODO: use
 
             using var streamReader = File.OpenText(fileInfo.FullName);
-            
+
             while ((nextLine = await streamReader.ReadLineAsync()) != null)
             {
-                var operation = _operationFactory.Create(nextLine);
+                operation = _operationFactory.Create(nextLine);
                 lineCounter++;
+
+                if (operation.OperationType == OperationType.Apply)
+                {
+                    // The apply instruction at the end of the file has been reached,
+                    // which has already been processed. Stop processing.
+                    break;
+                }
+
                 yield return operation;
             }
         }
 
-        private string GetLastLineOfFile()
+        private string GetLastLineOfFile(FileSystemInfo fileInfo)
         {
-            // TODO: implement:
-            // Read last n bytes of file into string
-            // Split at line break
-            // Return last of split
-            throw new NotImplementedException();
+            using Stream stream = File.OpenRead(fileInfo.FullName);
+
+            if (stream.Length == 0)
+            {
+                // The file is empty
+                return null;
+            }
+
+            // Start at end of file
+            stream.Position = stream.Length - 1;
+
+            // While we have not yet reached start of file, read bytes backwards until '\n' byte is encountered
+            while (stream.Position > 0)
+            {
+                stream.Position--;
+                var byteFromFile = stream.ReadByte();
+
+                if (byteFromFile < 0)
+                {
+                    // Someone must have emptied the file while it was being read
+                    throw new IOException("Error reading from file at " + fileInfo.FullName);
+                }
+
+                if (byteFromFile == '\n')
+                {
+                    // The new line was found; stream.Position is 1 after the '\n' char
+                    break;
+                }
+
+                stream.Position--;
+            }
+
+            // stream.Position will is immediately after the '\n' char or position 0 if no '\n' char
+            byte[] bytes = new BinaryReader(stream).ReadBytes((int) (stream.Length - stream.Position));
+
+            // TODO: handle non-ASCII encodings?
+            return System.Text.Encoding.ASCII.GetString(bytes);
         }
     }
 }
